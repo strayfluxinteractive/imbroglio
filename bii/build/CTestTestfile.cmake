ef8c4571d6ce7d376eaf0bef1f907179d70a94ce
strayfluxinteractive <strayfluxinteractive@gmail.com>
2015-04-26 20:27:19 +0000

    Promoted version.***travis***

---
+++ bii/build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /tmp/biicode_project/blocks/strayfluxinteractive/imbroglio/bii/cmake
+# Build directory: /tmp/biicode_project/blocks/strayfluxinteractive/imbroglio/bii/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
