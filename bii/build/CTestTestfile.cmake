ab2ca22277fb2148fb5135216f9b3f9ae18781d4
zephyrz4 <zephyrz2712@gmail.com>
2015-04-26 16:31:48 -0400

    Revert "Promoted version.***travis***"
    This reverts commit ef8c4571d6ce7d376eaf0bef1f907179d70a94ce.

--- bii/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /tmp/biicode_project/blocks/strayfluxinteractive/imbroglio/bii/cmake
-# Build directory: /tmp/biicode_project/blocks/strayfluxinteractive/imbroglio/bii/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
